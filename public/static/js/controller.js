5dc0f6bc110a144bbada3986a6600e5bbf24c54c
Kazuho Koganemaru <kazuho@hacarus.com>
2021-04-15 14:32:28 +0900

    modofied controller

--- public/static/js/controller.js
+++ public/static/js/controller.js
@@ -1,11 +1,23 @@
 const controller = {};
 
+const showController = () => {
+    const elem = document.getElementById("debug-controller");
+    let controllerStr = "";
+    Object.keys(controller).forEach(key => {
+        controllerStr += `${key}:${controller[key]} \n`;
+
+    });
+    elem.innerText = controllerStr
+}
+
 document.addEventListener('keydown', (event) => {
     console.log(`keydown:${event.key}`);
     controller[event.key] = true;
+    showController();
 });
 
 document.addEventListener('keyup', (event) => {
     console.log(`keyup:${event.key}`);
     controller[event.key] = false;
+    showController();
 });
